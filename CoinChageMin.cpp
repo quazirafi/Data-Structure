06700fc11bf047a54f85f5d8078296d99bc90077
saurav <sauravsust71@gmail.com>
2018-10-04 13:00:39 +0600

    Removed function from code

--- CoinChageMin.cpp
+++ CoinChageMin.cpp
@@ -1,25 +1,7 @@
 #include<bits/stdc++.h> 
 using namespace std; 
   
-int minCoins(int coins[], int m, int V) 
-{ 
-    int table[V+1]; 
-    table[0] = 0;  
-    for (int i=1; i<=V; i++) 
-        table[i] = 999999; 
- 
-    for (int i=1; i<=V; i++) 
-    { 
-        for (int j=0; j<m; j++) 
-          if (coins[j] <= i) 
-          { 
-              int sub_res = table[i-coins[j]]; 
-              if (sub_res != 999999 && sub_res + 1 < table[i]) 
-                  table[i] = sub_res + 1; 
-          } 
-    } 
-    return table[V]; 
-} 
+
 int main() 
 { 
     int coins[] =  {9, 6, 5, 1}; 
